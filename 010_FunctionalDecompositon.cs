b3c08fd88ee3dc63a9ea2815de2c0fae949486a5
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-17 23:19:12 +0100

    Initial Import

---
+++ 010_FunctionalDecompositon.cs
@@ -0,0 +1,15 @@
+using NUnit.Framework;
+
+namespace Tobii
+{
+    [TestFixture]
+    public class FunctionalDecompositon
+    {
+        [Test]
+        public void FlattenHierarchyFunctionally()
+        {
+            ///Do the folder hierarchy example with classical and functional
+        }
+        
+    }
+}
---
+++ 020_StreamProcessing.cs
@@ -0,0 +1,29 @@
+﻿using NUnit.Framework;
+
+namespace Tobii
+{
+    [TestFixture]
+    public class StreamProcessing
+    {
+        [Test]
+        public void UsingLinqStreamsToProcessInComingData()
+        {
+            ///Use blocking iterator + eventpushing thread + spliting of streams to 
+            /// implement multiple threads watching different filters and aggregations
+            /// of the generated point data.
+            /// 
+            /// Emit fixed points using the fixedpoint algorithm
+            /// 
+            /// Have separate stream for out of range data
+            /// 
+            /// separate stream for aggregating hotspot areas
+            /// 
+            /// Make the data timestamped so that it can be lazily evaluated later.
+        }
+
+        [Test]
+        public void UsingStreamsToImportExportData()
+        {
+        }
+    }
+}
