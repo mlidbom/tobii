dcb15c95366a5a9999b0689136c0759def48b6a2
Magnus Lidbom <magnus.lidbom@gmail.com>
2009-11-18 12:11:59 +0100

    Get some test files set up.

---
+++ 005_TheElephantInTheRoom.cs
@@ -0,0 +1,107 @@
+using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Void.Hierarchies;
+
+namespace Tobii
+{
+    /// <summary>
+    /// There is an elephant in the room. A large problem with the way 
+    /// that we write software. 
+    /// 
+    /// We are breaking the DRY and SRP principles of SOLID badly with 
+    /// most every function we write. 
+    /// 
+    /// Would you create a new List data type every time
+    /// you needed one? 
+    /// No? 
+    /// 
+    /// Then why do you do it with algorithms?
+    /// 
+    /// We do it again and again, numb from years of doing it because we had 
+    /// no good alternative. 
+    /// 
+    /// NOW WE DO!
+    /// 
+    /// </summary>
+    [TestFixture]
+    public class TheElephantInTheRoom
+    {
+        #region The wrong way
+
+        /// <summary>
+        /// This is about how I usually see code like this.
+        /// Unless the coder is afraid of recursion and makes 
+        /// it far uglier still that is!
+        /// </summary>
+        private static long SizeOfFolderClassic(string folder)
+        {
+            long result = 0;
+            string[] subFolders = Directory.GetDirectories(folder);
+            foreach (var subFolder in subFolders)
+            {
+                result += SizeOfFolderClassic(subFolder);
+            }
+
+            string[] files = Directory.GetFiles(folder);
+            foreach (var file in files)
+            {
+                result += new FileInfo(file).Length;
+            }
+
+            return result;
+        }
+
+        #endregion
+
+        #region And what's wrong with that?
+
+        // Fetching data, recursive descent tree walking, 
+        // transforming data, and data aggregation 
+        // is all baked into one big mess.
+        //
+        // Code should do one thing at a time in clear and easy to read steps.        
+        //        
+        // Wheels reinvented above: 
+        //  Recursive descent tree walking
+        //  Transforming data
+        //  Flattening data
+        //  Aggregating data
+        //
+        //In terms of SOLID this translates into violations of;
+        //1.SRP since the function is supposed to calculate the size of 
+        //a folder but takes upon itself to implement the wheels above.
+        //
+        //2. DRY since you are reinventing the wheel time and time again.
+        //
+
+        #endregion
+
+        #region The right way
+
+        private static long SizeOfFolderSane(string folder)
+        {
+            return folder.FlattenHierarchy(Directory.GetDirectories)
+                .SelectMany(dir => Directory.GetFiles(dir))
+                .Sum(file => new FileInfo(file).Length);
+        }
+
+        #endregion
+
+        #region Yes it works
+
+        [Test]
+        public void BothReturnNonSeroSizesThatAreTheSame()
+        {
+            var folder = @"C:\temp";
+            var sizeClassic = SizeOfFolderClassic(folder);
+            var sizeSane = SizeOfFolderSane(folder);
+
+            Assert.That(sizeClassic, Is.EqualTo(sizeSane));
+            Console.WriteLine(sizeSane); //Compare with total commander...            
+        }
+
+        #endregion
+    }
+}
--- 020_StreamProcessing.cs
+++ 020_StreamProcessing.cs
@@ -1,4 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+using Void;
+using Void.Linq;
 
 namespace Tobii
 {
@@ -25,5 +30,61 @@ namespace Tobii
         public void UsingStreamsToImportExportData()
         {
         }
+
+
+
+        [Test]
+        public void TestStabilityWithStatefulFunction()
+        {
+            GetCyclicPathPoints(500000)
+                .Where(IsStabilized())
+                .Count()
+                .Do(Console.WriteLine);
+        }
+
+        [Test]
+        public void TestStabilityViaChunking()
+        {
+            GetCyclicPathPoints(500000)
+                .ChopIntoSizesOf(10)
+                .Where(ChunkIsStable)
+                .SelectMany(me => me)
+                .Count()
+                .Do(Console.WriteLine);
+        }
+
+        protected bool ChunkIsStable(IEnumerable<Zipping.Pair<int, int>> points)
+        {
+            const int maxVariance = 50;
+            var xVariance = points.Max(p => p.First) - points.Min(p => p.First);
+            var yVariance = points.Max(p => p.Second) - points.Min(p => p.Second);
+            return xVariance < maxVariance && yVariance < maxVariance;
+        }
+
+
+        static Func<Zipping.Pair<int, int>, bool> IsStabilized()
+        {
+            const int maxVariance = 50;
+            var last10 = new LinkedList<Zipping.Pair<int, int>>(new Zipping.Pair<int, int>(0, 0).Repeat(10));
+            return currentPosition =>
+            {
+                last10.RemoveLast();
+                last10.AddFirst(currentPosition);
+                var xVariance = last10.Max(p => p.First) - last10.Min(p => p.First);
+                var yVariance = last10.Max(p => p.Second) - last10.Min(p => p.Second);
+                return xVariance < maxVariance && yVariance < maxVariance;
+            };
+        }
+
+        #region Very very ugly
+
+        private IEnumerable<Zipping.Pair<int, int>> GetCyclicPathPoints(int numberOfPoints)
+        {
+            return 1.Through(numberOfPoints)
+                .Select(me => Math.Abs((int)(1280 * Math.Sin(me / 100D))))
+                .Select(me => new Zipping.Pair<int, int>(me, me));
+        }
+
+        #endregion
     }
 }
