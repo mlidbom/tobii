4c92ffd89ec51441e21e9c758958e0dc01eabca5
Magnus Lidbom <magnus.lidbom@gmail.com>
2010-03-30 05:16:07 +0200

    Use some math tricks.

--- WpfAndReactiveExtensions/Collections/LimitedLengthList.cs
+++ WpfAndReactiveExtensions/Collections/LimitedLengthList.cs
@@ -1,11 +1,9 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using System.Linq;
 
 namespace WpfAndReactiveExtensions.Collections
 {
-    public class LimitedLengthList<T> : IEnumerable<T>
+    public class LimitedLengthList<T>
     {
         private readonly T[] _list;
         private int _currentPosition;
@@ -15,45 +13,12 @@ namespace WpfAndReactiveExtensions.Collections
             _list = Enumerable.Repeat(default(T), count).ToArray();
         }
 
-
-        private int RealIndex(int index)
-        {
-            if (index >= _list.Length)
-            {
-                throw new IndexOutOfRangeException();
-            }
-            int realIndex = index + _currentPosition;
-            return realIndex < _list.Length ? realIndex : realIndex - _list.Length;
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
-
-        public IEnumerator<T> GetEnumerator()
-        {
-            for (int index = 0; index < _list.Length; index++)
-            {
-                yield return this[index];
-            }
-        }
-
         public void Push(T item)
         {
             _list[_currentPosition] = item;
-            ShiftPosition();
+            _currentPosition = (_currentPosition + 1)%_list.Length;
         }
 
-        public T this[int index] { get { return _list[RealIndex(index)]; } }
-
-        private void ShiftPosition()
-        {
-            ++_currentPosition;
-            if (_currentPosition >= _list.Length)
-            {
-                _currentPosition = 0;
-            }
-        }
+        public T this[int index] { get { return _list[(_currentPosition + index)%_list.Length]; } }
     }
 }
